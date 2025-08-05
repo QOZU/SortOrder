0c60b41edff985ee8f71a7b942e115aa52a3b8ae
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-06 05:01:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/sortorder/main.go
@@ -0,0 +1,23 @@
+// cmd/sortorder/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"sortorder/internal/sortorder"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := sortorder.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
